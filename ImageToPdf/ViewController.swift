4f327978a04da0c33b9571b8ba65c29767ec5dc8
Marcelo Sampaio <marcelo@artlogica.com,br>
2018-12-13 10:09:08 -0200

    checkpoint commit 📸

--- ImageToPdf/ViewController.swift
+++ ImageToPdf/ViewController.swift
@@ -8,13 +8,97 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    // MARK: - Properties
+    private var imagePicker: UIImagePickerController!
+    
+    // Enums
+    enum ImageSource {
+        case photoLibrary
+        case camera
+    }
+    
+    // MARK: - Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var convertImageButton: UIBarButtonItem!
 
+    
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        convertImageButton.isEnabled = false
+        
     }
 
+    // MARK: - UI Action
+    @IBAction func takePicture(_ sender: Any) {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            selectImageFrom(.photoLibrary)
+            return
+        }
+        selectImageFrom(.camera)
+    }
+    
+    @IBAction func converImageToPdf(_ sender: Any) {
+        
+    }
+    
+    
+    
+    
+    // MARK: - Capture Image Helper
+    func selectImageFrom(_ source: ImageSource){
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        switch source {
+        case .camera:
+            imagePicker.sourceType = .camera
+        case .photoLibrary:
+            imagePicker.sourceType = .photoLibrary
+        }
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: - Image-PDF Converter Helper
+    func createPDFDataFromImage(image: UIImage) -> NSMutableData {
+        let pdfData = NSMutableData()
+        let imgView = UIImageView.init(image: image)
+        let imageRect = CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height)
+        UIGraphicsBeginPDFContextToData(pdfData, imageRect, nil)
+        UIGraphicsBeginPDFPage()
+        let context = UIGraphicsGetCurrentContext()
+        imgView.layer.render(in: context!)
+        UIGraphicsEndPDFContext()
+        
+        //try saving in doc dir to confirm:
+        let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last
+        let path = dir?.appendingPathComponent("file.pdf")
+        
+        do {
+            try pdfData.write(to: path!, options: NSData.WritingOptions.atomic)
+        } catch {
+            print("error catched")
+        }
+        
+        return pdfData
+    }
+    
+    
+    // MARK: - Image Picker Delegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        imagePicker.dismiss(animated: true, completion: nil)
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            print("❌ Image not found!")
+            return
+        }
+        self.imageView.image = selectedImage
+        convertImageButton.isEnabled = true
+    }
+    
+    
 
 }
 
